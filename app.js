5f95b0c42fdd2af7e97689cbc2ddf4afef5e698b
Harritone <yourrasik@gmail.com>
2020-09-13 11:08:54 +1000

    Added validation for input fields

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
@@ -136,19 +136,32 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   };
 
+  var updateBudget = function() {
+    // Calculate the budget
+    //
+    // Return the budget
+    //
+    // Display the budget on the UI
+
+  };
+
 
   var ctrlAddItem = function() {
     var input, newItem;
     // 1. Get the field input data
     input = UICtrl.getInput();
+
+    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
     // 2. Add the item to the budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     UIController.addListItem(newItem, input.type);
     // clear fields
     UIController.clearFields();
-    // 4. Calculate the budget
-    // 5. Display the budget on the UI
+
+    // calc and update budget
+    updateBudget();
+    }
   };
 
   return {
